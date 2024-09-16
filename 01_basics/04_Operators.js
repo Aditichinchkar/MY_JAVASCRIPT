d1695586775714f8aeb36682e3fbad876303aa51
Aditichinchkar <117304846+Aditichinchkar@users.noreply.github.com>
2024-09-16 14:06:51 +0000

    operators

---
+++ 01_basics/04_Operators.js
@@ -0,0 +1,22 @@
+/********************Operators*********************/
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="Aditi"
+let str2= " Chinchkar"
+
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+2+"3")
+// console.log("1"+2+2)
+
+let value=3
+let negvalue= -value
+console.log(negvalue)
+
+console.log(+true);
+console.log(+"")
