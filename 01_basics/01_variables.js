51a8078627e6a5ca3f50745b4dee0c978f19ee3b
Aditichinchkar <117304846+Aditichinchkar@users.noreply.github.com>
2024-09-16 11:06:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 1443
+let accountEmail="aahkj74g@gmail.com"
+var accountPassword = "12345"
+accountCity= "Solapur"
+
+//accountId=2// not allowed
+accountEmail="ddkfihhfkjd24@gmail.com"
+accountPassword= "5674"
+accountCity= "Jaipur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*Prefere not to use var due to block scope and functional scope*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as a newer vesion
+
+//alert(3+3)//we are using nodejs, not boowser
+
+console.log(3+3)
+console.log("hitesh")
+let name="aditi"
+let age="21"
+let state;
+let isloggedIn= false
+//number= 2 to power 53
+//bigint
+//boolean = true/ false
+//null = stand alone value
+//undefined= value is not defined yet
+//symbol == unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
